aaabfa192c24a22a57752e815819946b0e89a301
Adolfo Menezes Silva <menezes.adolfo@gmail.com>
2023-11-20 12:02:04 -0300

    Definido stricts_types

--- src/app/Constants/Priority.php
+++ src/app/Constants/Priority.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Constants;
 
 use Hyperf\Constants\AbstractConstants;
--- src/app/Resource/TaskResource.php
+++ src/app/Resource/TaskResource.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Resource;
 
 use Carbon\Carbon;
